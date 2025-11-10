28cb846625ccb7c5893ed67dae671f3d7bdbd3db
Aizo Craft <kariukiisaac911@gmail.com>
2025-11-10 19:11:31 +0300

    new commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',   
+  theme: {
+    extend: {
+      colors: {
+        primary: '#22c55e',
+      },
+    },
+  },
+  plugins: [],
+}
